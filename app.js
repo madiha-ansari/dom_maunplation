8f5fd5b22bbff277ce543092e0c7d9d2232e0ab0
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-09-29 21:57:45 +0500

    srdah kaprah part 1

---
+++ app.js
@@ -0,0 +1,28 @@
+// get Attributes
+// var div = document.querySelector("div");
+// console.log(div);
+
+// var value = div.getAttribute("id");
+// console.log(value);
+
+// var name = div.getAttribute("name");
+// console.log(name);
+
+// var para = document.querySelector("p");
+// var pars = para.getAttribute("class");  ////////////////////////
+// console.log(para.getAttribute("class"));
+// get Attributes complete 
+
+// setAttribute start 
+// var para = document.querySelector("p");
+// var pars = para.setAttribute("class","lorem");
+// console.log(pars);
+// setAttribute end
+
+// <!-- style  -->
+// <!-- node.style -->
+
+// var div = document.querySelector("div");
+// div.style.background="red";
+// div.style.fontSize="20px";
+// div.style.fontStyle=" italic";
